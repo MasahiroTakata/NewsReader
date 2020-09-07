e28691eb750882e29402cac899dade9000ec13c0
Masahiro Takata <takata005581016@gmail.com>
2020-09-07 17:55:39 +0900

    テーブルビューの作成

---
+++ NewsReader/ListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ListViewController.swift
+//  NewsReader
+//
+//  Created by 高田将弘 on 2020/09/07.
+//  Copyright © 2020 高田将弘. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 3
+    } // 表示するセルの数を３にする
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    } // 表示するセルを作成する
+}
