7a629271e1ac676be40d4033a239a58111df7496
Masahiro Takata <takata005581016@gmail.com>
2020-09-16 16:42:26 +0900

    RSSデータを取得して表示

---
+++ NewsReader/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  NewsReader
+//
+//  Created by 高田将弘 on 2020/09/12.
+//  Copyright © 2020 高田将弘. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title = ""
+    var link = ""
+}
--- NewsReader/ListViewController.swift
+++ NewsReader/ListViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
-class ListViewController: UITableViewController {
+class ListViewController: UITableViewController, XMLParserDelegate{
+    var parser:XMLParser!
+    var items = [Item]()
+    var item:Item?
+    var currentString = ""
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return 3
-    } // 表示するセルの数を３にする
+        return items.count // アプリに表示させるセル数
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        cell.textLabel!.text = "記事タイトル"
-        tableView.deleteRows(at: [indexPath], with: .automatic) // 記事を削除する
+        cell.textLabel?.text = items[indexPath.row].title
+        //tableView.deleteRows(at: [indexPath], with: .automatic) // 記事を削除する
         
         return cell
     } // 表示するセルを作成する
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        startDownload()
+    }
+    
+    func startDownload() {
+        self.items = []
+        
+        if let url = URL(string: "https://wired.jp/rssfeeder/"){
+            if let parser = XMLParser(contentsOf: url){
+                self.parser = parser
+                self.parser.delegate = self
+                self.parser.parse()
+            }
+        }
+    }
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String: String]) {
+        self.currentString = ""
+        
+        if elementName == "item" {
+            self.item = Item()
+        }
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String){
+        self.currentString += string
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?){
+        switch elementName {
+            case "title": self.item?.title = currentString
+            case "link": self.item?.link = currentString
+            case "item": self.items.append(self.item!)
+            default: break
+        }
+    }
+    
+    func parserDidEndDocument(_ parser: XMLParser) {
+        self.tableView.reloadData()
+    }
 }
