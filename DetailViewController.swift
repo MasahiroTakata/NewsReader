f2aff8c8afb392814fade4d22c6b7dcabde704b3
Masahiro Takata <takata005581016@gmail.com>
2020-09-26 22:09:32 +0900

    コメント追加

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -14,12 +14,12 @@ class DetailViewController : UIViewController {
     @IBOutlet weak var webView: WKWebView!
     var link:String!
     
-    override func viewDidLoad(){
+    override func viewDidLoad(){ // webビューに対象のWebサイトを表示する
         super.viewDidLoad()
         
         if let url = URL(string: self.link){
-            let request = URLRequest(url: url)
-            self.webView.load(request)
+            let request = URLRequest(url: url) // requestインスタンスを作成
+            self.webView.load(request) // requestに格納されたurlを表示する
         }
     }
 }
--- NewsReader/ListViewController.swift
+++ NewsReader/ListViewController.swift
@@ -14,18 +14,16 @@ class ListViewController: UITableViewController, XMLParserDelegate{
     var item:Item?
     var currentString = ""
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{ // セルの数を設定する
         return items.count // アプリに表示させるセル数
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{ // セルの内容を設定する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) // 画面範囲から消えたセルを再利用するメソッド この時、どのセルを再利用するのかを指定する
         cell.textLabel?.text = items[indexPath.row].title
-        //tableView.deleteRows(at: [indexPath], with: .automatic) // 記事を削除する
         
-        return cell
-    } // 表示するセルを作成する
+        return cell // 表示するセルを作成する
+    }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -35,29 +33,30 @@ class ListViewController: UITableViewController, XMLParserDelegate{
     func startDownload() {
         self.items = []
         
-        if let url = URL(string: "https://wired.jp/rssfeeder/"){
-            if let parser = XMLParser(contentsOf: url){
-                self.parser = parser
+        if let url = URL(string: "https://wired.jp/rssfeeder/"){ // オプショナルバインディング、iPhoneアプリ開発では頻繁に使う
+            if let parser = XMLParser(contentsOf: url){ // これもオプショナルバインディング nilの場合は式の結果はfalseになり、if文の中身は実行されない
+                self.parser = parser // parserメソッド呼び出し
                 self.parser.delegate = self
-                self.parser.parse()
+                self.parser.parse() // データを解析するメソッド
             }
         }
     }
     
+    // 必要なデータのみを取り出すメソッド（要素名の開始タグが見つかる毎に呼び出される）
     func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String: String]) {
         self.currentString = ""
         
-        if elementName == "item" {
-            self.item = Item()
+        if elementName == "item" { // 変数elementNameにはダウンロードしたRSSのデータの要素名が入っている
+            self.item = Item() // タグ要素が「item」の時、ニュース記事を入れる箱を用意する
         }
     }
     
     func parser(_ parser: XMLParser, foundCharacters string: String){
-        self.currentString += string
+        self.currentString += string // 見つかった記事の内容を格納する
     }
     
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?){
-        switch elementName {
+        switch elementName { // 要素名をチェックそして、要素ごとに処理を分ける
             case "title": self.item?.title = currentString
             case "link": self.item?.link = currentString
             case "item": self.items.append(self.item!)
@@ -66,15 +65,15 @@ class ListViewController: UITableViewController, XMLParserDelegate{
     }
     
     func parserDidEndDocument(_ parser: XMLParser) {
-        self.tableView.reloadData()
+        self.tableView.reloadData() // テーブルビューの内容を更新して、取得したニュースの記事を画面に表示する
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
         if let indexPath = self.tableView.indexPathForSelectedRow {
-            let item = items[indexPath.row]
-            let controller = segue.destination as! DetailViewController
-            controller.title = item.title
-            controller.link = item.link
+            let item = items[indexPath.row] // ユーザーがタップしたセルのindexPathを取得、items配列から該当する記事を取得
+            let controller = segue.destination as! DetailViewController // 遷移先のビューコントローラーを格納
+            controller.title = item.title // 遷移先のtitleプロパティに記事のタイトルを格納
+            controller.link = item.link // 記事のURLを格納
         }
     }
 }
